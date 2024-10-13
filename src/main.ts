3654d93c35d27fa20b885a464f52d4caee0c284e
ishachury20 <35016233+ishachury20@users.noreply.github.com>
2024-10-12 21:39:02 -0700

    step 1 complete added canvas and other minor edits

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,23 @@ import "./style.css";
 const APP_NAME = "Hello Dino!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// document.title = APP_NAME;
+// app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerText = APP_NAME; 
+app.appendChild(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256; 
+canvas.height = 256; 
+canvas.style.border = "2px solid #4682B4"; 
+canvas.style.borderRadius = "5px";
+canvas.style.boxShadow = "2px 2px #244c6e"; 
+
+app.appendChild(canvas);
+
+const board = canvas.getContext("2d")!;
+board.fillStyle = "#FFFFFF"; 
+board.fillRect(0, 0, canvas.width, canvas.height); 
+
