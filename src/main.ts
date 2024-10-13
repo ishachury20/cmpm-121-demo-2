3e4e92ea9d81027c6a9033b46df463ebf07c289d
ishachury20 <35016233+ishachury20@users.noreply.github.com>
2024-10-12 22:32:42 -0700

    step 1 complete added border and drop shadow in css

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,50 @@ const board = canvas.getContext("2d")!;
 board.fillStyle = "#FFFFFF"; 
 board.fillRect(0, 0, canvas.width, canvas.height); 
 
+let isDrawing = false; 
+let x = 0;
+let y = 0;
+
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+      drawLine(board, x, y, e.offsetX, e.offsetY);
+      x = e.offsetX;
+      y = e.offsetY;
+    }
+});
+
+globalThis.addEventListener("mouseup", (e) => {
+    drawLine(board, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+);
+
+function drawLine(board: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    board.beginPath();
+    board.strokeStyle = "black";
+    board.lineWidth = 1;
+    board.moveTo(x1, y1);
+    board.lineTo(x2, y2);
+    board.stroke();
+    board.closePath();
+}; 
+
+const undoAllButton = document.createElement("button"); 
+undoAllButton.innerHTML = "Undo All Edits"; 
+undoAllButton.style.margin = "25px"
+
+undoAllButton.onclick = () => {
+    board.clearRect(0, 0, canvas.width, canvas.height);
+    board.fillStyle = "#FFFFFF";
+    board.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+app.append(undoAllButton);
